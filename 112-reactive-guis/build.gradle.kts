5841b3cd11db40b5c4cfec9106d2ecabd93d9f3c
pianinator <113355583+pianinator@users.noreply.github.com>
2026-09-22 16:39:24 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@83ccdfad3aa808fcd10acc6ef3cfc9f0eea7ae06 🚀

--- 112-reactive-guis/build.gradle.kts
+++ 112-reactive-guis/build.gradle.kts
@@ -14,7 +14,7 @@ tasks.javadoc {
 
 dependencies {
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.10.4")
-    implementation("org.slf4j:slf4j-api:2.0.19")
+    implementation("org.slf4j:slf4j-api:2.0.20")
     //  JOOL: Java 8+ Fluent API for JDK 8+ Streams
     implementation("org.jooq:jool:0.9.15")
     // Logback backend for SLF4J
